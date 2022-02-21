6169100d1973c4125f482bd06f087a1ea225c07b
Fernando Paiva <fernandopaiva@techamazon.tech>
2022-02-21 09:40:48 -0300

    feat: now it's possible to sort by year, month or day

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -1,11 +1,14 @@
 import { PathLike } from 'fs'
 
-type filesList = {
+type FilesList = {
   name: string
   path: PathLike
   createdAt: Date
 }[]
 
+type SortByOptions = 'year' | 'month' | 'day'
+
 export {
-  filesList
+  FilesList,
+  SortByOptions
 }
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import { statSync, mkdirSync, readdirSync, PathLike, renameSync } from 'fs'
 
 import { log } from '@shared'
-import { filesList } from '@types'
+import { FilesList, SortByOptions } from '@types'
 
-const getFilesList = (path: PathLike): filesList | null => {
+const getFilesList = (path: PathLike): FilesList | null => {
   try {
     return readdirSync(path, { withFileTypes: true })
       .filter(dirent => dirent.isFile())
@@ -35,7 +35,7 @@ const months = [
   'Dezembro'
 ]
 
-const createFolders = (path: PathLike, filesList: filesList) => {
+const createFolders = (path: PathLike, filesList: FilesList, sortBy: SortByOptions) => {
   const dates = Array.from(
     filesList.map(file => ({
       day: `${file.createdAt.getDate()}`,
@@ -48,12 +48,19 @@ const createFolders = (path: PathLike, filesList: filesList) => {
 
   dates.forEach(date => {
     try {
-      mkdirSync(
-        `${path}/${date.year}/${months[Number(date.month) - 1]}/${date.day}`,
-        {
-          recursive: true
-        }
-      )
+      let dirPath = ''
+
+      if (sortBy === 'year') {
+        dirPath = `${path}/${date.year}`
+      } else if (sortBy === 'month') {
+        dirPath = `${path}/${date.year}/${months[Number(date.month) - 1]}`
+      } else if (sortBy === 'day') {
+        dirPath = `${path}/${date.year}/${months[Number(date.month) - 1]}/${date.day}`
+      }
+
+      mkdirSync(dirPath, {
+        recursive: true
+      })
     } catch (err: any) {
       log.error(err)
       errors.push(err)
@@ -67,7 +74,7 @@ const createFolders = (path: PathLike, filesList: filesList) => {
   }
 }
 
-const moveFiles = (path: PathLike, filesList: filesList) => {
+const moveFiles = (path: PathLike, filesList: FilesList, sortBy: SortByOptions) => {
   const errors = []
   
   filesList.forEach(file => {
@@ -76,9 +83,19 @@ const moveFiles = (path: PathLike, filesList: filesList) => {
     const day = file.createdAt.getDate()
 
     try {
+      let dirPath = ''
+
+      if (sortBy === 'year') {
+        dirPath = `${path}/${year}/${file.name}`
+      } else if (sortBy === 'month') {
+        dirPath = `${path}/${year}/${month}/${file.name}`
+      } else if (sortBy === 'day') {
+        dirPath = `${path}/${year}/${month}/${day}/${file.name}`
+      }
+
       renameSync(
         file.path,
-        `${path}/${year}/${month}/${day}/${file.name}`
+        dirPath
       )
 
     } catch (err: any) {
@@ -94,7 +111,7 @@ const moveFiles = (path: PathLike, filesList: filesList) => {
   }
 }
 
-const main = (path: PathLike) => {
+const main = (path: PathLike, sortBy: SortByOptions) => {
   log.info('Listando arquivos')
 
   const filesList = getFilesList(path)
@@ -102,10 +119,10 @@ const main = (path: PathLike) => {
   if (filesList) {
     log.info('Criando pastas')
 
-    if (createFolders(path, filesList)) {
+    if (createFolders(path, filesList, sortBy)) {
       log.info('Movendo arquivos')
 
-      if (moveFiles(path, filesList)) {
+      if (moveFiles(path, filesList, sortBy)) {
         log.info('Arquivos organizados')
       } else {
         log.error(new Error('Ocorreu um erro'))
@@ -116,8 +133,15 @@ const main = (path: PathLike) => {
 
 const run = () => {
   const path = process.argv[2]
-  console.clear()
-  main(path)
+  const sortBy = process.argv[3]
+
+  if (['year', 'month', 'day'].includes(sortBy)) {
+    console.clear()
+    main(path, sortBy as SortByOptions)
+  } else {
+    console.clear()
+    main(path, 'month')
+  }
 }
 
 run()
